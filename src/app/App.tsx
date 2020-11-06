69f4020157cf4b3b2b4c797875fee671c83e47f9
jaygraygray <jeremygray001@gmail.com>
2020-11-06 08:55:08 -0500

    prototyping stages of drawing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,47 @@
-import React from "react";
+// @ts-nocheck
+import React, { useEffect, useRef, useCallback, useState } from "react";
+
+
+// click once, place FirstComponent
+// capture mouse move events
+// second click, place SecondComponent
+
+const FirstComp = () => <div>I am first!</div>
+const SecondComp = () => <div>Second!!!</div>
+const Renderer = ({ status }) => {
+  console.log(">>status", status);
+  return (
+    <>
+      {status === "placing" && <FirstComp />}
+      {status === "drawing" && <SecondComp />}
+    </>
+  )
+}
 
 export const App = (props) => {
+  const [drawState, setDrawState] = useState("placing");
+  const bodyRef = useRef()
+
+  const onClick = useCallback(() => {
+    if (drawState === "placing") {
+      setDrawState("drawing")
+    }
+
+    if (drawState === "drawing") {
+      setDrawState("placing")
+    }
+  }, [drawState]);
+
+  const captureMove = useCallback((e) => {
+    if (drawState === "drawing") {
+      console.log(">>> mouse event", e)
+    }
+  }, [drawState]);
+
 
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      woof
+    <div style={{ height: "100vh", width: "100vw" }} onClick={onClick} onMouseMove={captureMove} ref={bodyRef}>
+      <Renderer status={drawState} />
     </div>    
   );
 }
