4a847774b3ee22470486535304fd2df35fd37fae
jaygraygray <jeremygray001@gmail.com>
2020-11-28 17:38:25 -0500

    test icon imports: we gucci

--- src/app/icons/index.ts
+++ src/app/icons/index.ts
@@ -1,5 +1,11 @@
 import Arrow from './src/Arrow';
+import Finger from './src/Finger';
+import Pencil from './src/Pencil';
+import Settings from './src/Settings';
+import Star from './src/Star';
 
-export {
-  Arrow as ArrowIcon
-}
+export { Arrow as ArrowIcon };
+export { Finger as FingerIcon };
+export { Pencil as PencilIcon };
+export { Settings as SettingsIcon };
+export { Star as StarIcon };
